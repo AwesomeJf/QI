437e883319261b9f86f091ae3262738efbe72e7b
finlu <2388757097@qq.com>
2019-05-30 00:03:01 +0800

    提交数据库创建的SQL文件

---
+++ db.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS `user`, `paper`, `subject`, `option`;
+
+CREATE TABLE IF NOT EXISTS `user`
+(
+    id            INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username      CHAR(30) NOT NULL,
+    password      CHAR(30) NOT NULL,
+    isAdmin       BOOLEAN  DEFAULT FALSE,
+    createTime    DATETIME DEFAULT CURRENT_TIMESTAMP,
+    lastLoginTime DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS `paper`
+(
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title       CHAR(50) NOT NULL,
+    isPublish   BOOLEAN DEFAULT TRUE,
+    publishTime DATETIME,
+    endTime     DATETIME,
+    userId      INTEGER  NOT NULL,
+    FOREIGN KEY (userId) REFERENCES `user` (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS `subject`
+(
+    id      INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `index` INT DEFAULT 0,
+    title   CHAR(50) NOT NULL,
+    type    CHAR(30) NOT NULL,
+    paperId INTEGER  NOT NULL,
+    FOREIGN KEY (paperId) REFERENCES `paper` (id)
+);
+
+CREATE TABLE IF NOT EXISTS `option`
+(
+    id        INTEGER PRIMARY KEY AUTO_INCREMENT,
+    content   TEXT(500) NOT NULL,
+    subjectId INTEGER   NOT NULL,
+    FOREIGN KEY (subjectId) REFERENCES `subject` (id)
+);
